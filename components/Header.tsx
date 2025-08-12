1700aaa44274479de0f7215e0a6ccd6950d03adb
Amber Li <amber@human-nyc.com>
2025-08-11 20:41:11 -0400

    added mobile header

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,7 @@ export default function Header({ header } : { header : NonNullable<HeaderQueryRe
   };
 
   useMotionValueEvent(scrollY, "change", (current) => {
-    if (current > 5) {
+    if (current > 10) {
       setMinimizeHeader(true)
     } else {
       setMinimizeHeader(false)
@@ -31,7 +31,7 @@ export default function Header({ header } : { header : NonNullable<HeaderQueryRe
   return (
     <>
       <motion.div 
-        className="max-md:hidden top-0 px-[0.3125rem] py-2 mx-auto"
+        className="top-[0.6125rem] px-[0.3125rem] py-2 mx-auto"
         initial="maximize"
         variants={variants}
         animate={minimizeHeader ? "minimize" : "maximize"}
@@ -39,12 +39,16 @@ export default function Header({ header } : { header : NonNullable<HeaderQueryRe
         <Logo />
       </motion.div>
       <header className="sticky top-0 px-5 py-3">
-        <div className="max-md:hidden flex justify-between">
-          <div className="flex gap-8">
-            {navList && navList.map((link: LinkValue, i: number) => <Link key={i} link={link} className="nav">{link.text}</Link>)}          
-          </div>
+        <div className="flex justify-between">
           <div>
-            <span className="nav">View A B C</span>
+            <div className="max-md:hidden flex gap-8">
+              {navList && navList.map((link: LinkValue, i: number) => <Link key={i} link={link} className="nav">{link.text}</Link>)}          
+            </div>
+            <span className="md:hidden nav">Menu</span>   
+          </div>      
+          <div>
+            <span className="max-md:hidden nav">View A B C</span>
+            <span className="md:hidden nav">Change View</span>
           </div>
         </div>
       </header>
