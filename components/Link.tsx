4195a496e0be6be803744e99e3da3b259dc5e8b4
Amber Li <amber@human-nyc.com>
2025-08-11 20:04:22 -0400

    fixed import statement

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,6 +1,6 @@
 import { resolveHref } from '@/sanity/lib/utils';
 import { default as NextLink } from 'next/link';
-import { Link as SanityLink, type LinkProps } from 'sanity-plugin-link-field/component';
+import { Link as SanityLink, type LinkProps } from 'sanity-plugin-link-field/src/component';
 
 export function Link(props: LinkProps) {
   return (
